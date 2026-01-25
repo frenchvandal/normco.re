a66bc6cd8b13062feca6963aa8c06f3946d594a5
李北洛 Philippe <42499534+frenchvandal@users.noreply.github.com>
2026-01-25 20:13:52 +0800

    test: move utility tests to root

---
+++ tests/text_test.ts
@@ -0,0 +1,16 @@
+import { assertEquals } from "jsr:@std/assert@1.0.8";
+
+import { slugify } from "../src/_utilities/text.ts";
+
+Deno.test("slugify trims and lowercases", () => {
+  assertEquals(slugify("  Hello World  "), "hello-world");
+});
+
+Deno.test("slugify strips punctuation and collapses whitespace", () => {
+  assertEquals(slugify("Hello, world!!!"), "hello-world");
+  assertEquals(slugify("Multiple   spaces"), "multiple-spaces");
+});
+
+Deno.test("slugify collapses duplicate hyphens", () => {
+  assertEquals(slugify("Hello -- world"), "hello-world");
+});
