e8c19c85925ec5da4b740b88cbf8f876e2746760
李北洛 Philippe <42499534+frenchvandal@users.noreply.github.com>
2026-03-05 20:58:00 +0800

    second commit

--- _config.ts
+++ _config.ts
@@ -1,8 +1,9 @@
 import lume from "lume/mod.ts";
+import blog from "blog/mod.ts";
 
-const site = lume({
-  src: "./src",
-  location: new URL("https://normco.re"),
-});
+const site = lume(  {src: "./src",
+  location: new URL("https://normco.re"),});
+
+site.use(blog());
 
 export default site;
