ff56208d3a45f926b1de4606a19daf3901304cd4
李北洛 Philippe <42499534+frenchvandal@users.noreply.github.com>
2026-03-20 23:17:52 +0800

    fix: use appcompat activity for android locales

--- apps/android/app/src/main/java/re/phiphi/android/MainActivity.kt
+++ apps/android/app/src/main/java/re/phiphi/android/MainActivity.kt
@@ -1,15 +1,15 @@
 package re.phiphi.android
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 import re.phiphi.android.ui.PhiphiApp
 import re.phiphi.android.ui.theme.PhiphiTheme
 
 @AndroidEntryPoint
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
